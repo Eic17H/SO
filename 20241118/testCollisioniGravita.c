3ac350f1ea19025fa451ac69a35c4bff487e2b30
Eic17H <139933730+Eic17H@users.noreply.github.com>
2024-11-18 19:15:22 +0200

    Tasto reset

--- 20241118/testCollisioniGravita.c
+++ 20241118/testCollisioniGravita.c
@@ -10,8 +10,11 @@ keypad(stdscr, 1);
 box﻿ (stdscr﻿ , ACS_VLINE﻿ , ACS_HLINE﻿ )﻿ ;
 int x ﻿ = COLS﻿ /﻿ 2﻿ , y ﻿ = 0 , c﻿ ;
 mvaddch﻿ (﻿ y﻿ , x﻿ , OBJECT﻿ )﻿ ; refresh()﻿ ;
+char input = ' ';
 int spd=0;
 while(1){
+input = getch();
+if(input == 'a') y = 0;
 mvaddch(y,x,' ');
 spd += G;
 y += spd;
