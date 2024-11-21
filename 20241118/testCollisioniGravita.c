95c46ca0f4cd62a35ee347bb3e539e5b32d3d698
Eic17H <139933730+Eic17H@users.noreply.github.com>
2024-11-21 17:33:52 +0200

    Ordine

--- 20241118/testCollisioniGravita.c
+++ 20241118/testCollisioniGravita.c
@@ -4,21 +4,24 @@
 #define G -9.8
 #define ELASTICITY 0.5
 
-int main() {
- initscr(); noecho(); curs_set(0); cbreak();
-keypad(stdscr, 1);
-box﻿ (stdscr﻿ , ACS_VLINE﻿ , ACS_HLINE﻿ )﻿ ;
-int x ﻿ = COLS﻿ /﻿ 2﻿ , y ﻿ = 0 , c﻿ ;
-mvaddch﻿ (﻿ y﻿ , x﻿ , OBJECT﻿ )﻿ ; refresh()﻿ ;
-char input = ' ';
-int spd=0;
-while(1){
-input = getch();
-if(input == 'a') y = 0;
-mvaddch(y,x,' ');
-spd += G;
-y += spd;
-if(y>=LINES) spd = -spd/2;
-mvaddch(y, x, OBJECT);
-refresh();
-}
+int main()
+{
+    // Cose normali da fare sempre
+    int x=COLS/2, y=0, c;
+    char input = ' ';
+    initscr(); noecho(); curs_set(0); cbreak(); keypad(stdscr, 1); box(stdscr, ACS_VLINE, ACS_HLINE); mvaddch(y, x, OBJECT); refresh();
+    
+    int spd = 0;
+    while (1)
+    {
+        input = getch();
+        if (input == 'a')
+            y = 0;
+        mvaddch(y, x, ' ');
+        spd += G;
+        y += spd;
+        if (y >= LINES)
+            spd = -spd / 2;
+        mvaddch(y, x, OBJECT);
+        refresh();
+    }
