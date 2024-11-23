0c61cb6ba01ca8a264e0b51039277486690e8264
Eic17H <eic17h+github@gmail.com>
2024-11-23 03:22:31 +0100

    Test collisioni gravità convertito in float

--- 20241118/testCollisioniGravita.c
+++ 20241118/testCollisioniGravita.c
@@ -6,7 +6,7 @@
 #include <sys/wait.h>
 #define OBJECT '#'
 // Positivo=giù
-#define G 1
+#define G 0.5
 // Tra 1.13 e 1.2 risulta 0 perdita di energia, forse per errori di arrotondamento non è 1
 // Sopra 1.2 rimbalza anche sopra e guadagna velocità verticale ogni volta e poi si rompe
 #define ELASTICITY 1.1
@@ -14,27 +14,28 @@
 // Un carattere si sposta a destra a velocità costante e rimbalza elasticamente
 int main()
 {
-    int x=2, y=2, c;
+    float x=2, y=2;
+    int c;
     // Se devo aggiungere input
     char input = ' ';
     // Cose normali da fare sempre
-    initscr(); noecho(); curs_set(0); cbreak(); keypad(stdscr, 1); box(stdscr, ACS_VLINE, ACS_HLINE); mvaddch(y, x, OBJECT); refresh();
+    initscr(); noecho(); curs_set(0); cbreak(); keypad(stdscr, 1); box(stdscr, ACS_VLINE, ACS_HLINE); mvaddch((int)y, (int)x, OBJECT); refresh();
     // Cambia carattere a ogni "frame"
     int i = 0;
     // Velocità corrente, parte da 0
-    int spd = 0;
+    float spd = 0;
     while (1){
 	// I caratteri vanno dalla A alla Z e ricominciano
 	i = (i+1)%26;
-        mvaddch(y, x, i+'A');
+        mvaddch((int)y, (int)x, i+'A');
 	// Formula del movimento uniformemente accelerato, diciamo
         spd += G; y += spd;
 	// Se tocca il fondo, rimbalza elasticamente (=perdendo energia nell'impatto)
-        if (y >= LINES-2 || y <= 1)
+        if ((int)y >= LINES-2 || (int)y <= 1)
             spd = (int) -(((float)spd)*(ELASTICITY));
 	// Visualizza, aspetta, sposta a destra
         refresh();
-	sleep(0.1);
+	sleep(1);
 	x++;
     }
 }
